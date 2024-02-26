1a56f9f7d724ee57281b152d8b25e3f7c119cd28
Brian <brianwaweru58@gmail.com>
2024-02-26 20:55:44 +0100

    first Commit

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+
+export default function DigitalClock(){
+    return(
+    <div className="clock-container">
+            <div className="clock">
+                <span>00:00:00</span>
+            </div>
+    </div>)
+}
